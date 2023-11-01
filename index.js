8037c8083aa86f8c849055359860fbc84da9a9d1
lineMarques <aline.marques18@gmail.com>
2023-11-01 16:52:58 -0300

    consumo de api via Fecth

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var btBuscarMoeda = document.getElementById("idBtnMoedas");
 
-btBuscarMoeda.onclick = function () {
-    buscaMoeda();
+/* Consumo de API com AJAX */ 
+
+/* var btBuscarMoedaAjax = document.getElementById("idBtnMoedasAjax");
+
+btBuscarMoedaAjax.onclick = function () {
+  buscaMoedaAjax();
 };
 
-function buscaMoeda() {
+function buscaMoedaAjax() {
   var xhr = new XMLHttpRequest();
 
   xhr.open(
@@ -23,7 +26,25 @@ function buscaMoeda() {
 function imprimirMoeda(moedaJSON) {
   let moedaOBJ = JSON.parse(moedaJSON);
 
-  moedaOBJ.value.forEach(element => {
-    console.table('AJAX', element.simbolo)
-  });       
+  moedaOBJ.value.forEach((element) => {
+    console.table("AJAX", element.simbolo);
+  });
+} */ 
+
+
+/* consumo de api via fetch */
+var btBuscarMoedaFetch = document.getElementById("idBtnMoedasFetch");
+
+btBuscarMoedaFetch.onclick = async function () {
+  let moedas = await buscaMoedaFetch();
+  console.log(moedas);
+};
+
+/* consumo de api com fetch */
+async function buscaMoedaFetch() {
+  var resposta = await fetch(
+    "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/Moedas?$top=100&$format=json&$select=simbolo,nomeFormatado,tipoMoeda"
+  );
+
+  return resposta.json();
 }
